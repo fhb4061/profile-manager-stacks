14e71c6b0dab1b9b3fb11f6c6a3d0882d70ef056
Tane <jonathanlopeti@gmail.com>
2026-08-06 15:56:17 +1000

    inline policy and tighten some resource access

--- lib/github-role-stack.ts
+++ lib/github-role-stack.ts
@@ -6,6 +6,30 @@ export class GithubRoleStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        // create Identity provider first
+        const idProvider = new aws_iam.OidcProviderNative(this, "GithubProvider", {
+            url: "https://token.actions.githubusercontent.com",
+            clientIds: ["sts.amazonaws.com"]
+        });
+
+
+        // create new Role entrusted to idProvider with ECR permissions inlined
+        const githubRole = new aws_iam.Role(this, "GithubRole", {
+            roleName: "github-action-role",
+            description: "Allow github action workflows to access certain AWS resources",
+            assumedBy: new aws_iam.WebIdentityPrincipal(
+                idProvider.openIdConnectProviderArn,
+                {
+                    StringEquals: {
+                        "token.actions.githubusercontent.com:aud": ["sts.amazonaws.com"]
+                    },
+                    StringLike: {
+                        "token.actions.githubusercontent.com:sub": "repo:fhb4061/profile-lambda:ref:refs/heads/master"
+                    }
+                }
+            )
+        });
+
         // statements for reading/writing images
         const ecrStatement = new aws_iam.PolicyStatement({
             sid: 'ecrRepoAccess',
@@ -21,7 +45,7 @@ export class GithubRoleStack extends cdk.Stack {
                 "ecr:CompleteLayerUpload",
                 "ecr:PutImage"
             ],
-            resources: [`arn:aws:ecr:*:${process.env.CDK_DEFAULT_ACCOUNT}:repository/*`]
+            resources: [`arn:aws:ecr:${this.region}:${this.account}:repository/profile-*`]
         });
 
         // allow login into respository
@@ -33,40 +57,8 @@ export class GithubRoleStack extends cdk.Stack {
             resources: ["*"]
         });
 
-        // create managed policy
-        const ecrManagedPolicy = new aws_iam.ManagedPolicy(this, "GithubActionPolicy", {
-            managedPolicyName: "ecr-read-write-policy",
-            description: "Allows read and write to any ECR in my account",
-            statements: [ecrStatement, authEcrStatement]
-        });
-
-        // create Identity provider first
-        const idProvider = new aws_iam.OidcProviderNative(this, "GithubProvider", {
-            url: "https://token.actions.githubusercontent.com",
-            clientIds: ["sts.amazonaws.com"]
-        });
-
-
-        // create new Role entrusted to idProvider with managed policies in place
-        const githubRole = new aws_iam.Role(this, "GithubRole", {
-            roleName: "github-action-role",
-            description: "Allow github action workflows to access certain AWS resources",
-            managedPolicies: [ecrManagedPolicy],
-            assumedBy: new aws_iam.WebIdentityPrincipal(
-                idProvider.openIdConnectProviderArn,
-                {
-                    StringEquals: {
-                        "token.actions.githubusercontent.com:aud": ["sts.amazonaws.com"]
-                    },
-                    StringLike: {
-                        "token.actions.githubusercontent.com:sub": [
-                            "repo:fhb4061/*",
-                            "repo:fhb4061/*"
-                        ]
-                    }
-                }
-            )
-        });
+        githubRole.addToPolicy(ecrStatement);
+        githubRole.addToPolicy(authEcrStatement);
 
         new cdk.CfnOutput(this, 'RoleArn', {
             value: githubRole.roleArn,
--- test/github-role-stack.test.ts
+++ test/github-role-stack.test.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib/core';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Match, Template } from 'aws-cdk-lib/assertions';
 import * as GithubRoleStack from '../lib/github-role-stack';
 
 const app = new cdk.App();
@@ -16,6 +16,26 @@ describe("Github role, policy, and provider test", () => {
         });
     });
 
+    test("role can only be assumed by profile-lambda repo on master branch", () => {
+        template.hasResourceProperties("AWS::IAM::Role", {
+            AssumeRolePolicyDocument: {
+                Statement: [
+                    {
+                        Action: "sts:AssumeRoleWithWebIdentity",
+                        Condition: {
+                            StringEquals: {
+                                "token.actions.githubusercontent.com:aud": ["sts.amazonaws.com"]
+                            },
+                            StringLike: {
+                                "token.actions.githubusercontent.com:sub": "repo:fhb4061/profile-lambda:ref:refs/heads/master"
+                            }
+                        }
+                    }
+                ]
+            }
+        });
+    });
+
     test("Github OIDC provider created", () => {
         template.hasResource("AWS::IAM::OIDCProvider", {
             Properties: {
@@ -25,13 +45,56 @@ describe("Github role, policy, and provider test", () => {
         });
     });
 
-    test("Github Policy created", () => {
-        template.hasResource("AWS::IAM::ManagedPolicy", {
-            Properties: {
-                ManagedPolicyName: "ecr-read-write-policy",
+    test("ECR permissions are inlined on the role, no managed policy", () => {
+        template.resourceCountIs("AWS::IAM::ManagedPolicy", 0);
+        template.hasResourceProperties("AWS::IAM::Policy", {
+            PolicyDocument: {
+                Statement: [
+                    {
+                        Sid: "ecrRepoAccess",
+                        Action: [
+                            "ecr:BatchGetImage",
+                            "ecr:GetDownloadUrlForLayer",
+                            "ecr:BatchCheckLayerAvailability",
+                            "ecr:DescribeRepositories",
+                            "ecr:InitiateLayerUpload",
+                            "ecr:UploadLayerPart",
+                            "ecr:CompleteLayerUpload",
+                            "ecr:PutImage"
+                        ],
+                        Effect: "Allow"
+                    },
+                    {
+                        Sid: "ecrGlobalAccess",
+                        Action: "ecr:GetAuthorizationToken",
+                        Effect: "Allow",
+                        Resource: "*"
+                    }
+                ]
             }
         });
     })
+
+    test("ECR repo access is scoped to profile-* repos in the stack's region and account", () => {
+        template.hasResourceProperties("AWS::IAM::Policy", {
+            PolicyDocument: {
+                Statement: Match.arrayWith([
+                    Match.objectLike({
+                        Sid: "ecrRepoAccess",
+                        Resource: {
+                            "Fn::Join": ["", [
+                                "arn:aws:ecr:",
+                                { Ref: "AWS::Region" },
+                                ":",
+                                { Ref: "AWS::AccountId" },
+                                ":repository/profile-*"
+                            ]]
+                        }
+                    })
+                ])
+            }
+        });
+    });
 
     test("output exposes the role ARN for GitHub Actions workflow config", () => {
         template.hasOutput('RoleArn', {});
